48b4724c0db4c037e56acb0642abf0f31ea1b884
thomashsimm <thomas.simm@gmail.com>
2022-05-11 19:32:16 +0100

    Update GolfVideoStreamLit.py

--- GolfVideoStreamLit.py
+++ GolfVideoStreamLit.py
@@ -307,7 +307,7 @@ def createImages(fila,nomS,events):
 def loadStuff():
     stra = uploaded_files
     
-    seq_length=16
+    seq_length=25
     input_size=120
     
     ds = SampleVideo(stra, 
